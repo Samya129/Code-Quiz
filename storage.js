0d0a9c90265a0ea90c791db32588c44d494c1bd3
Sam129 <Samyaf020@gmail.com>
2020-12-03 03:22:50 -0500

    initial elements created.

---
+++ storage.js
@@ -0,0 +1,7 @@
+var initialsInput = document.querySelector("#initials");
+var highscoreInput = document.querySelector("#highscore");
+var signUpButton = document.querySelector("#sign-up");
+var msgDiv = document.querySelector("#msg");
+var userInitialsSpan = document.querySelector("#user-initials");
+var userHighscoreSpan = document.querySelector("#user-highscore");
+
